6ee59701050eb32f4ca9751b1f2a2973fdcbe419
Dang LE Binh <dang.lebinh@elca.vn>
2022-08-04 18:04:30 +0700

    init angular

---
+++ my-first-app/src/app/cokpit/cokpit.component.ts
@@ -0,0 +1,25 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-cokpit',
+  templateUrl: './cokpit.component.html',
+  styleUrls: ['./cokpit.component.css']
+})
+export class CokpitComponent implements OnInit {
+  @Output('srvCreated') serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  newServerContent = '';
+  // newServerName = '';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  
+  onAddServer(nameInput: HTMLInputElement) {
+    this.serverCreated.emit({serverName: nameInput.value, serverContent: this.newServerContent});
+  }
+
+  onAddBlueprint(nameInput: HTMLInputElement) {
+    this.blueprintCreated.emit({serverName: nameInput.value, serverContent: this.newServerContent});
+  }
+}
